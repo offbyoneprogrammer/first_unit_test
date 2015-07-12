580cae9817ca5cecb7bb09d3553413ca07596ba9
Martin <git@offbyoneprogrammer.com>
2015-07-12 17:54:46 +0200

    Step1

--- WhenWillYouDie/WhenWillYouDie/DieYearCalculator.cs
+++ WhenWillYouDie/WhenWillYouDie/DieYearCalculator.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace WhenWillYouDie
 {
-    static class DieYearCalculator
+    public static class DieYearCalculator
     {
         public static int CalculateDyingYear(String name)
         {
---
+++ WhenWillYouDie/WhenWillYouDieUnitTests/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WhenWillYouDie;
+
+namespace WhenWillYouDieUnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void EmptyStringTest()
+        {
+            int result = DieYearCalculator.CalculateDyingYear("");
+            Assert.AreEqual(0, result);
+        }
+    }
+}
